b2ccb07657aeb17fffac58f888d4cefdf5309276
SandaminiObadage <hashiniobadage6030@gmail.com>
2025-06-26 12:05:41 +0530

    arrow making

--- posMasterV3/src/components/Header.jsx
+++ posMasterV3/src/components/Header.jsx
@@ -19,9 +19,18 @@ function Header() {
             <BreadCrumb/>
           </div>
           <div className="flex items-center">
-            <div className="flex flex-row items-center gap-6 ms-3">
+            <div className="flex flex-row items-center gap-2 ms-3">
 
               <img className="w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user profile image"/>
+              <svg
+    className="w-4 h-4 text-gray-500"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+  </svg>
             </div>
           </div>
         </div>
