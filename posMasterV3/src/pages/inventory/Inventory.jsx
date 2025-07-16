1cd3cd281a66e754cfa26ef5e81d6e9aa3dfb919
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-07-16 13:36:32 +0530

    updated search bar styling

--- posMasterV3/src/pages/inventory/Inventory.jsx
+++ posMasterV3/src/pages/inventory/Inventory.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import InventorySidebar from "./Inventory_sidebar";
 import InventoryCard from "../../frontend/components/Inventory_card";
@@ -7,7 +7,7 @@ import bananaImg from "../../assets/Inventory_banana.png";
 import AddItem from "./AddItem.jsx";
 import NotFound from "../../assets/nonicons_not-found-16.png";
 import InventoryConfig from "./InventoryConfig";
-import InventoryReport from "./InventoryReport"; 
+import InventoryReport from "./InventoryReport";
 import { useOutletContext } from "react-router-dom";
 
 function Inventory() {
@@ -21,7 +21,7 @@ function Inventory() {
     setActiveSection(activeSection);
   }, [activeSection, setActiveSection]);
 
-   const handleSectionChange = (section) => {
+  const handleSectionChange = (section) => {
     setLocalActiveSection(section);
     setActiveSection(section);
   };
@@ -214,7 +214,7 @@ function Inventory() {
       (searchCategory === "All" || item.category === searchCategory) &&
       item.name.toLowerCase().includes(search.toLowerCase())
   );
- 
+
 
   // helper function to toggle Tailwind visibility between sections
   const isVisible = (section) =>
@@ -223,17 +223,17 @@ function Inventory() {
   return (
     <div id="inv-background" className="flex h-screen bg-[#EBEBEB] -ml-12">
       {/* sidebar (left) */}
-     <InventorySidebar
+      <InventorySidebar
         activeSection={activeSection}
         onViewInvClick={() => handleSectionChange("view-inventory")}
         onAddItemClick={() => handleSectionChange("add-item")}
         onConfigClick={() => handleSectionChange("inventory-config")}
         onCReportClick={() => handleSectionChange("inventory-report")}
       />
-      
+
 
       <main className="flex-1 bg-[#F3F3F3] h-[calc(100vh-6rem)] relative">
-        
+
         {/* View Inventory */}
         <div className={isVisible("view-inventory")}>
           <nav className="w-full flex justify-between py-4 px-10 bg-white gap-6 mb-4">
@@ -242,13 +242,17 @@ function Inventory() {
                 type="text"
                 value={search}
                 onChange={handleSearch}
-                placeholder="Search your item here..."
-                className="flex-1 px-3 py-2 bg-white focus:outline-none"
+                placeholder="Search Your Items here"
+                className="flex-1 px-3 py-2 bg-transparent focus:outline-none"
               />
-              <button className="px-10 py-2 bg-[#1A318C] text-white">
+              <button
+                onClick={handleSearch}
+                className="px-10 py-2 bg-[#1A318C] text-white"
+              >
                 Search
               </button>
             </div>
+
             <select
               value={searchCategory}
               onChange={(e) => setSearchCategory(e.target.value)}
