986a04f07960ee3ec2a85a69ee6fb09a78fc03ae
Udith <developer.udithsandaruwan@gmail.com>
2025-08-06 20:06:01 +0530

    Implementation of Inventory view - filter side bar

--- posMasterV3/src/pages/inventory/ViewInventory.jsx
+++ posMasterV3/src/pages/inventory/ViewInventory.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function ViewInventory() {
   return (
-   <div className="h-[45rem] overflow-y-scroll bg-white p-8 rounded">
+    <div className="h-[45rem] overflow-y-scroll bg-whit
+   e p-8 rounded">
       <h2 className="text-2xl font-bold mb-4">View Inventory</h2>
       {/* Add your inventory table or content here */}
     </div>
