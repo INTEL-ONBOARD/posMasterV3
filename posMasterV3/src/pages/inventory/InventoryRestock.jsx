8a71956a01eaadcdc3cc0b62258681970def9294
B1N4L <163591915+B1N4L@users.noreply.github.com>
2025-08-12 10:30:18 +0530

    update: new sections added

--- posMasterV3/src/pages/inventory/InventoryRestock.jsx
+++ posMasterV3/src/pages/inventory/InventoryRestock.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function InventoryRestock() {
   return (
-    <div>InventoryRestock page will be available soon</div>
+<div className="flex bg-black w-full h-[calc(100vh-2rem)] relative">
+  {/* form section (left) */}
+  <div className="bg-gray-200 w-[calc(28rem)] h-full">
+
+  </div>
+  {/* main transaction section (mid) */}
+  {/* they don't pay me enough for this😞 */}
+  <div className="bg-gray-500 w-[calc(45rem)] h-full">
+
+  </div>
+  {/*button section () */}
+  <div className="bg-gray-300 border-yellow-300 w-[calc(32rem)] h-full">
+    
+  </div>
+</div>
+
   )
 }
 
