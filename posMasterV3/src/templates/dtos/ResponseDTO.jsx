7e8e310531cf6c9ff982b2e4efa51c5bf6f81c5f
vidunisalitha <194532004+vidunisalitha@users.noreply.github.com>
2025-10-03 20:50:06 +0530

    Keep original feature/common version for ResponseDTO after merge

--- posMasterV3/src/templates/dtos/ResponseDTO.jsx
+++ posMasterV3/src/templates/dtos/ResponseDTO.jsx
@@ -1,5 +1,4 @@
 export default class ResponseDTO {
-<<<<<<< HEAD
     // Declaring private fields as private
     #data;
     #message;
@@ -9,22 +8,10 @@ export default class ResponseDTO {
         this.#data = data;
         this.#message = message;
         this.#status = status;
-=======
-    // Declaring fields as public
-    data;
-    message;
-    status;
-
-    constructor({ status, message, data }) {
-        this.data = data;
-        this.message = message;
-        this.status = status;
->>>>>>> bf562e173213cfdbd85e43de8cfb9ac98fa784e0
     }
 
     // Getters
     get data() {
-<<<<<<< HEAD
         return this.#data;
     }
 
@@ -34,22 +21,10 @@ export default class ResponseDTO {
 
     get status() {
         return this.#status;
-=======
-        return this.data;
-    }
-
-    get message() {
-        return this.message;
-    }
-
-    get status() {
-        return this.status;
->>>>>>> bf562e173213cfdbd85e43de8cfb9ac98fa784e0
     }
 
     // Setters
     set data(value) {
-<<<<<<< HEAD
         this.#data = value;
     }
 
@@ -59,39 +34,18 @@ export default class ResponseDTO {
 
     set status(value) {
         this.#status = value;
-=======
-        this.data = value;
-    }
-
-    set message(value) {
-        this.message = value;
-    }
-
-    set status(value) {
-        this.status = value;
->>>>>>> bf562e173213cfdbd85e43de8cfb9ac98fa784e0
     }
 
     // For testing
     get showResponse() {
-<<<<<<< HEAD
         return `${this.#data} (${this.#message}) (${this.#status})`;
-=======
-        return `${this.data} (${this.message}) (${this.status})`;
->>>>>>> bf562e173213cfdbd85e43de8cfb9ac98fa784e0
     }
 
     toJSON() {
         return {
-<<<<<<< HEAD
             data: this.#data,
             message: this.#message,
             status: this.#status,
-=======
-            data: this.data,
-            message: this.message,
-            status: this.status,
->>>>>>> bf562e173213cfdbd85e43de8cfb9ac98fa784e0
         };
     }
 }
