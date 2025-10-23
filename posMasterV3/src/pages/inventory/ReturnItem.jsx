6f587cb4ee66562110d2e6f4d40f9ae56a7c2df2
chandisarandeni <chandisarandeni2003@gmail.com>
2025-10-23 15:19:10 +0530

    feat: optimize the UI  ✅

--- posMasterV3/src/pages/inventory/ReturnItem.jsx
+++ posMasterV3/src/pages/inventory/ReturnItem.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import barcodeImg from "../../assets/barcode.png";
+import { ChevronDown, Printer, ChevronUp } from "lucide-react";
 
 function ReturnItem() {
   const [openFormBlock, setOpenFormBlock] = useState("item");
@@ -7,52 +8,46 @@ function ReturnItem() {
   return (
     <div className="flex bg-black w-full h-[calc(100vh-2rem)] relative">
       {/* Form section left */}
-      <div className="bg-gray-300 w-[calc(28rem)] h-[calc(100vh-2rem)] p-2 z-10">
-        {/* 56 is the correct height */}
-        <div className="flex flex-col h-[46rem] gap-3">
+      <div className="bg-gray-300 w-[28rem] h-full p-4 z-10">
+        <div className="flex flex-col h-full gap-6">
           {/* Item description box */}
-          <div className="border rounded bg-white">
-            {/* ------------------------------------------------------------------------ */}
-            {/* ▼ item description block ▼ */}
-            <div className="border rounded bg-white">
+          <div className="border rounded bg-white p-4">
+            {/* Item description block */}
+            <div className="border rounded bg-white mb-4">
               <button
-                className="w-full flex justify-between items-center bg-white px-4 py-2 text-lg font-bold"
+                className="w-full flex justify-between items-center bg-white px-4 py-3 text-lg font-bold"
                 onClick={() =>
                   setOpenFormBlock(openFormBlock === "item" ? "" : "item")
                 }
               >
                 <span className="text-gray-400">ITEM DESCRIPTION</span>
+                {openFormBlock == "item" ? <ChevronUp /> : <ChevronDown />}
               </button>
               {openFormBlock === "item" && (
-                <div className="mx-4">
-                  <div className="flex flex-row px-4 items-center max-h-[12rem]">
+                <div className="mt-4">
+                  <div className="flex items-center gap-6">
                     <div>
                       <img
                         alt="Barcode"
                         className="w-[100px] object-contain"
                         src={barcodeImg}
                       />
-                      {/* SKU */}
                       <p className="text-sm font-semibold text-gray-800 mt-2">
                         SKU: 123456789
                       </p>
                     </div>
-                    {/* Vertical black line separator */}
-                    <div className="flex flex-col m-10">
-                      <div className="grid grid-cols-2 gap-x-1 gap-y-1">
-                        {/* Name */}
+                    <div className="flex flex-col">
+                      <div className="grid grid-cols-2 gap-y-2 gap-x-4">
                         <p className="text-sm font-semibold text-gray-800">
                           Name
                         </p>
                         <p className="text-sm text-gray-700">Sample Item</p>
 
-                        {/* Category */}
                         <p className="text-sm font-semibold text-gray-800">
                           Category
                         </p>
                         <p className="text-sm text-gray-700">Sample Category</p>
 
-                        {/* Current Qty */}
                         <p className="text-sm font-semibold text-gray-800">
                           Current Qty
                         </p>
@@ -63,198 +58,179 @@ function ReturnItem() {
                 </div>
               )}
             </div>
-            {/* ------------------------------------------------------------------------ */}
 
             {/* Stock Description */}
-            <div className="border rounded bg-white mt-2">
+            <div className="border rounded bg-white mb-4">
               <button
-                className="w-full flex justify-between items-center bg-white px-4 py-2 text-lg font-bold"
+                className="w-full flex justify-between items-center bg-white px-4 py-3 text-lg font-bold"
                 onClick={() =>
                   setOpenFormBlock(openFormBlock === "stock" ? "" : "stock")
                 }
               >
                 <span className="text-gray-400">STOCK DESCRIPTION</span>
+                {openFormBlock == "item" ? <ChevronUp /> : <ChevronDown />}
               </button>
               {openFormBlock === "stock" && (
-                <div className="flex flex-col gap-2">
-                  <div className="grid grid-cols-2 gap-4">
-                    {/* Batch Code */}
+                <div className="mt-4">
+                  <div className="grid grid-cols-2 gap-6">
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Batch Code
                       </label>
                       <input
                         type="text"
                         name="batch_code"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Quantity */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Quantity
                       </label>
                       <input
                         type="number"
                         name="quantity"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Lower Threshold Rate */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Lower Threshold Rate
                       </label>
                       <input
                         type="number"
                         name="lower_threshold"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Availability */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Availability
                       </label>
                       <select
                         name="availability"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       >
                         <option value="in_stock">In Stock</option>
                         <option value="out_of_stock">Out of Stock</option>
                         <option value="pre_order">Pre-order</option>
                       </select>
                     </div>
-                    {/* Stock Price */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Stock Price
                       </label>
                       <input
                         type="number"
                         name="stock_price"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Retail Price */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Retail Price
                       </label>
                       <input
                         type="number"
                         name="retail_price"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Expire Date */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Expire Date
                       </label>
                       <input
                         type="date"
                         name="expire_date"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Discount % */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Discount (%)
                       </label>
                       <input
                         type="number"
                         name="discount_percentage"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-                    {/* Recent Batch code changes */}
-                    <p className="text-sm font-semibold text-gray-800 mt-4">
-                      Recent Batch Code Changes
+                  </div>
+                  <p className="text-sm font-semibold text-gray-800 mt-6">
+                    Recent Batch Code Changes
+                  </p>
+                  <div className="flex flex-col gap-3 overflow-y-auto h-[10rem] mt-2">
+                    <p className="text-sm text-gray-700 italic">
+                      No batch code changes
                     </p>
-                    <br />
-                    <div className="flex flex-col gap-3 overflow-y-auto overflow-x-hidden h-[13rem] pr-2">
-                      <p className="text-sm text-gray-700 italic">
-                        No batch code changes
-                      </p>
-                    </div>
                   </div>
                 </div>
               )}
             </div>
-            {/* ----------------------------------------------------------------------------- */}
 
-            {/* Return Descrition */}
-            <div className="border rounded bg-white mt-2">
+            {/* Return Description */}
+            <div className="border rounded bg-white">
               <button
-                className="w-full flex justify-between items-center bg-white px-4 py-2 text-lg font-bold"
+                className="w-full flex justify-between items-center bg-white px-4 py-3 text-lg font-bold"
                 onClick={() =>
                   setOpenFormBlock(openFormBlock === "return" ? "" : "return")
                 }
               >
                 <span className="text-gray-400">RETURN DESCRIPTION</span>
+                {openFormBlock == "item" ? <ChevronUp /> : <ChevronDown />}
               </button>
               {openFormBlock === "return" && (
-                <div className="flex flex-col gap-2">
-                  <div className="grid grid-cols-2 gap-4">
-                    {/* SKU */}
+                <div className="mt-4">
+                  <div className="grid grid-cols-2 gap-6">
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         SKU
                       </label>
                       <input
                         type="text"
                         name="sku"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
-
-                    {/* Dispose Quantity */}
                     <div>
-                      <label className="block text-sm font-medium text-gray-400 mb-1">
+                      <label className="block text-sm font-medium text-gray-400 mb-2">
                         Dispose Quantity
                       </label>
                       <input
                         type="number"
                         name="dispose_quantity"
-                        className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                        className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       />
                     </div>
                   </div>
-
-                  {/* Return Date */}
-                  <div>
-                    <label className="block text-sm font-medium text-gray-400 mb-1">
+                  <div className="mt-4">
+                    <label className="block text-sm font-medium text-gray-400 mb-2">
                       Return Date
                     </label>
                     <input
                       type="date"
                       name="return_date"
-                      className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                      className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                   </div>
-
-                  {/* Description */}
-                  <div>
-                    <label className="block text-sm font-medium text-gray-400 mb-1">
+                  <div className="mt-4">
+                    <label className="block text-sm font-medium text-gray-400 mb-2">
                       Description
                     </label>
                     <textarea
                       name="description"
                       rows="4"
-                      className="w-full px-3 py-2 border bg-[#F8F8F8] border-[#EBEBEB] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                      className="w-full px-3 py-2 border bg-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     ></textarea>
                   </div>
                 </div>
               )}
             </div>
-            {/* ----------------------------------------------------------------------------- */}
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default ReturnItem;
