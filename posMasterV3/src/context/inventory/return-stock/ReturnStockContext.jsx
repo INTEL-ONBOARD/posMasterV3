c27e45982a78593aee9a177a423b2be61240010a
chandisarandeni <chandisarandeni2003@gmail.com>
2025-11-19 20:26:13 +0530

    feat: batch code retrieve ✅

--- posMasterV3/src/context/inventory/return-stock/ReturnStockContext.jsx
+++ posMasterV3/src/context/inventory/return-stock/ReturnStockContext.jsx
@@ -1,9 +1,9 @@
 import apiClient from "../../../api/apiClient";
 
-export const fetchStockContext = async () => {
+export const fetchStockContext = async (sku) => {
   try {
-    // Fetch batch code by skua -> pass sku
-    const batchCodeRes = await apiClient.get("/api/restocks/stock-data");
+    // Fetch batch code by SKU -> pass sku as parameter
+    const batchCodeRes = await apiClient.get(`/api/restocks/stock-data/${sku}`);
 
     return {
       batchCodes: batchCodeRes.data,
